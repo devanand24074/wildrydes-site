7d86f79b49cada46588e0837bb809f8eb9ded1bf
devanand24074 <devanand.b2022@vitstudent.ac.in>
2024-11-21 21:04:46 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_X4Fm4RIc6',// e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '38l5nslrdlc03vtjlln01khdut', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
